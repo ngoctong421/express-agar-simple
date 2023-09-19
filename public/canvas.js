c5708a41be04ba4eaae85e054b86b50b85fee81e
Ngoc Tong <ngoctong421@gmail.com>
2023-09-19 14:27:51 +0700

    draw the player

--- public/canvas.js
+++ public/canvas.js
@@ -1,1 +1,16 @@
-const init = () => {};
+const init = () => {
+  draw();
+};
+
+let randomX = Math.floor(500 * Math.random() + 10);
+let randomY = Math.floor(500 * Math.random() + 10);
+
+context.beginPath();
+context.fillStyle = 'rgb(255, 0, 0)';
+context.arc(randomX, randomY, 10, 0, Math.PI * 2);
+context.fill();
+context.lineWidth = 3;
+context.strokeStyle = 'rgb(0, 255, 0)';
+context.stroke();
+
+const draw = () => {};
