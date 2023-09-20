363e0d5768731ebd83e4b865dab35d18173a2f81
Ngoc Tong <ngoctong421@gmail.com>
2023-09-20 15:24:18 +0700

    disconnect

--- src/socket/index.js
+++ src/socket/index.js
@@ -10,12 +10,12 @@ const {
 
 const orbs = [];
 const settings = {
-  defaultNumberOfOrbs: 500,
+  defaultNumberOfOrbs: 5000,
   defaultSpeed: 6,
   defaultSize: 6,
   defaultZoom: 1.5,
-  worldWidth: 500,
-  worldHeight: 500,
+  worldWidth: 5000,
+  worldHeight: 5000,
   defaultGenericOrbSize: 5,
 };
 const players = [];
@@ -124,6 +124,14 @@ io.on('connect', (socket) => {
   });
 
   socket.on('disconnect', () => {
+    for (let i = 0; i < players.length; i++) {
+      if ((players[i].socketId = player.socketId)) {
+        players.splice(i, 1, {});
+        playerForUsers.splice(i, 1, {});
+        break;
+      }
+    }
+
     if (players.length === 0) {
       clearInterval(tickTockInterval);
     }
